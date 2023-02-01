44e309018e240c8ff87e069fb505bb0f16e0429e
Bureau <gausebastien@hotmail.fr>
2023-02-01 18:33:22 +0100

    Titres ok, pas d'image

--- Backend/app.js
+++ Backend/app.js
@@ -7,7 +7,11 @@ const swaggerUi = require('swagger-ui-express')
 const yaml = require('yamljs')
 const swaggerDocs = yaml.load('swagger.yaml')
 const app = express()
-app.use(cors())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(helmet());
--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,30 +1,64 @@
+const reponse = await fetch('http://localhost:5678/api/works'); // On va chercher le Json
+const objets = await reponse.json(); // On crée une const objets qu'on asssocie au résultat renvoyé par le json. Equivalent à "1ere ligne".then.pieces => pieces.json();
+if (reponse.ok) {
+    console.log(objets)
+  }
+// pour tous les objets, on utilise un for of. On utilise aussi une fonction gernererPieces comme ça on purra la rappeler plusieurs fois pour mettre à jour la page web
+function genererObjets(objets) {
+  for (let i = 0; i < objets.length; i++){
+
+    const article = objets[i];
+    // Récupération de l'élément du DOM qui accueillera les objets
+    const sectionGallery = document.querySelector(".gallery");
+            // Création de la balise figure qui englobe image et titre
+    const objetElement = document.createElement("figure")
+    const imageElement = document.createElement("img");
+    imageElement.src = article.imageUrl;
+    console.log(article.imageUrl)
+    const titreElement = document.createElement("figcaption");
+    titreElement.innerText = article.title;
+
+    sectionGallery.appendChild(objetElement);
+    objetElement.appendChild(imageElement);
+    objetElement.appendChild(titreElement);
+  }
+}
+
+genererObjets(objets);
 
 
-fetch("http://localhost:5678/api/works")
-  .then(function (response) {
-    if (response.ok) {
-      console.log(response.json())
-      let element = document.getElementById('portfolio').getElementsByClassName('gallery');
-      console.log(element)
-      let figure = document.createElement('figure');
-      let img =document.createElement('img');
-      let figcaption =document.createElement('figcaption');
+
+
+
+
+
+
+
+// fetch("http://localhost:5678/api/works")
+//   .then(function (response) {
+//     if (response.ok) {
+//       console.log(response.json())
+//       let element = document.getElementById('portfolio').getElementsByClassName('gallery');
+//       console.log(element)
+//       let figure = document.createElement('figure');
+//       let img =document.createElement('img');
+//       let figcaption =document.createElement('figcaption');
       
-      img.src = "assets/images/abajour-tahina.png"
-      figcaption.textContent = "res[0].title"
+//       img.src = "assets/images/abajour-tahina.png"
+//       figcaption.textContent = "res[0].title"
 
-      figure.appendChild(img)
-      figure.appendChild(figcaption)
-      element[0].appendChild(figure)
+//       figure.appendChild(img)
+//       figure.appendChild(figcaption)
+//       element[0].appendChild(figure)
 
-    }
-    else {
-      console.log("mauvaise réponse")
-    }
-  })
-  .catch(error => { // Attrape l'erreur
-    console.log("error : " + error) // Affiche error + la nature de l'erreur
-  })
+//     }
+//     else {
+//       console.log("mauvaise réponse")
+//     }
+//   })
+//   .catch(error => { // Attrape l'erreur
+//     console.log("error : " + error) // Affiche error + la nature de l'erreur
+//   })
 
 
 
