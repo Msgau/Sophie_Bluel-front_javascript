c9798f8eabc7ffb7f8a0bf1a9f4b16872247b399
Bureau <gausebastien@hotmail.fr>
2023-03-03 11:33:51 +0100

    fonction delete ciblée terminée

--- FrontEnd/modifierDom.js
+++ FrontEnd/modifierDom.js
@@ -1,4 +1,4 @@
-export function ajouterImage() {
+export async function ajouterImage() {
 
     console.log("ajouter image")
     const getToken = localStorage.getItem("token")
@@ -6,25 +6,28 @@ export function ajouterImage() {
     formulaireImageUp.addEventListener("submit", function (event) {
         event.preventDefault();
         // Convertir le nom de l'image en binaire
-        const imageBinary = querySelector("[name=upfile]")
-        const textToBinary = (imageBinary = '') => {
-            let res = '';
-            res = imageBinary.split('').map(char => {
-                return char.charCodeAt(0).toString(2);
-            }).join(' ');
-            return res;
-        };
-        console.log(textToBinary(imageBinary));
+        // const imageBinary = querySelector("[name=upfile]")
+        // const textToBinary = (imageBinary = '') => {
+        //     let res = '';
+        //     res = imageBinary.split('').map(char => {
+        //         return char.charCodeAt(0).toString(2);
+        //     }).join(' ');
+        //     return res;
+        // };
+        // console.log(textToBinary(imageBinary));
         // console.log(imageBinary.value);
+        const lectureImage = document.querySelector("name=upfile")
+        
         const image = {
-            image: event.target.querySelector("[name=upfile]").value,
+            image: event.target.querySelector("[name=upfile]").value, // La propriété value contient la valeur saisie par l'utilisateur sur la page web
+            // image: imageBinary.value
             title: event.target.querySelector("[name=title]").value,
             category: event.target.querySelector("[name=categorie]").value
         }
         const chargeUtile = JSON.stringify(image);
-        console.log(image)
+        console.log(chargeUtile)
 
-        fetch("http://localhost:5678/api/works", {
+        await fetch("http://localhost:5678/api/works", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -36,8 +39,109 @@ export function ajouterImage() {
 }
 
 
+export async function fetchPostWork(formData) {
+    const bearerToken = localStorage.getItem("token")
+    try {
+      const response = await fetch("http://localhost:5678/api/works",{
+        method: "POST",
+        headers: {
+          'authorization': `Bearer ${bearerToken}`,
+          'accept':'application/json',
+        },
+        body: formData   
+      })
+      const rep = await response.json() 
+        resetAll()
+        addWorkWindow.style.display="none" 
+    } catch (err) {
+      console.log('Une erreur est survenue',err)
+    }
+  }
+
+export function addWork() { 
+    const postForm = document.querySelector("[name=upfile]")
+    postForm.addEventListener("submit",(e)=> {
+      e.preventDefault()
+      if (inputFile.files[0].size < 4000000) {
+        const formData = new FormData(postForm)
+        fetchPostWork(formData)
+      }
+    }) 
+  }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export function deleteOne(){
+    const imagesElements = document.querySelectorAll(".trash");
+    const bearerToken = localStorage.getItem("token")
+    for (let i=0; i < imagesElements.length; i++){
+      imagesElements[i].addEventListener("click", function(event){
+        const id = event.target.dataset.id;
+        console.log(id)
+        fetch(`http://localhost:5678/api/works/${id}`, {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${bearerToken}`
+          }})
+      })
+    }
+  }
+
+// export function deleteOne(){
+//     const imagesElements = document.querySelectorAll(".trash");
+//     const bearerToken = localStorage.getItem("token")
+//     for (let i=0; i < imagesElements.length; i++){
+//       imagesElements[i].addEventListener("click", function(event){
+//         const id = event.target.dataset.id;
+//         fetch(`http://localhost:5678/api/works/${id}`, {
+//           method: "DELETE",
+//           headers: {
+//             "Content-Type": "application/json",
+//             "Authorization": `Bearer ${bearerToken}`
+//           }})
+//       })
+//     }
+//   }
+
+
+// function deleteWork(data) {
+//     const trashes = document.querySelectorAll(".fa-trash-can")
+//     for (trashCan of trashes) {
+//       trashCan.addEventListener("click",(e)=> {
+//         const index =((Array.from(trashes)).indexOf(e.target))
+//         const id = (data[index].id)
+//         fetch(`http://localhost:5678/api/works/${id}`,{
+//           method:"DELETE",
+//           headers: {
+//             'authorization': `Bearer ${userToken}`,
+//           }
+//         })
+//         .then (response => {
+//           if (response.status === 204 ) {
+//             modalWindow.style.display="none";
+//             thumbnailWindow.style.display="flex";
+//             createDeleteThumbnail(data[index])
+//             eventCloseThumbnail()     
+//           } 
+//         })            
+//       }) 
+//     } 
+//   }
 
 
 // export function supprimerImage(imageId) {
